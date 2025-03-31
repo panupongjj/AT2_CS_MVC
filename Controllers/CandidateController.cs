367929fafd43fbd45644dcf4db3e9e435a638fad
panupongjj <panupong.jangjun@gmail.com>
2025-03-31 13:52:54 +1100

    Add Controller - candidate

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AT2_CS_MVC.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
