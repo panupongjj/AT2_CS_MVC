0326e139eecc5d6c7bac6fe266ec9465098fb753
panupongjj <panupong.jangjun@gmail.com>
2025-03-31 13:45:44 +1100

    Add Model - applicant
    added new model for applicant data

---
+++ Models/Applicant.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace AT2_CS_MVC.Models
+{
+    public class Applicant
+    {
+        [Key]
+        public long Id { get; set; }
+        public string name { get; set; }
+        public DateTime dob { get; set; }
+
+        public List<SelectListItem> qulification = new List<SelectListItem>();
+       
+        public float gpa { get; set; }
+        public string university { get; set; }
+
+        //[Display(Name = "Heading")]
+        //[Required(ErrorMessage = "Please give the Title")]
+        //[StringLength(50, MinimumLength = 10, ErrorMessage = "SSSSSSSSSSSSSSSSSSSS")]
+
+    }
+}
