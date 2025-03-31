f072411d2a82ad263fc933f78b8e84cedde90384
panupongjj <panupong.jangjun@gmail.com>
2025-03-31 13:12:30 +1100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AT2_CS_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
